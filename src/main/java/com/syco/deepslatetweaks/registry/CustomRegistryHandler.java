dfcda9793adcec8c5e27bd919f01dd897aee6a3a
Peter Pulcini <peter.pulcini89@gmail.com>
2023-04-28 00:11:11 -0400

    Updated 1.19.2

--- src/main/java/com/syco/deepslatetweaks/registry/CustomRegistryHandler.java
+++ src/main/java/com/syco/deepslatetweaks/registry/CustomRegistryHandler.java
@@ -1,6 +1,7 @@
 package com.syco.deepslatetweaks.registry;
 
 import com.syco.deepslatetweaks.DeepslateTweaks;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -11,41 +12,38 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ObjectHolder;
 
 @Mod.EventBusSubscriber(modid = DeepslateTweaks.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class CustomRegistryHandler{
+public class CustomRegistryHandler {
 
     //REGISTERING ALL BLOCKS
-    @ObjectHolder("minecraft:deepslate")
+    @ObjectHolder(registryName = "minecraft:block", value = "minecraft:deepslate")
     public static final Block DEEPSLATE_BLOCK = null;
 
-    @ObjectHolder("minecraft:cobbled_deepslate")
+    @ObjectHolder(registryName = "minecraft:block", value = "minecraft:cobbled_deepslate")
     public static final Block COBBLED_DEEPSLATE_BLOCK = null;
 
     @SubscribeEvent
-    public static void registerBlocks(RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll(
-                new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).requiresCorrectToolForDrops().strength(1.5F, 6.0F).sound(SoundType.DEEPSLATE)).setRegistryName("minecraft:deepslate"),
-                new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(1.5F, 6.0F)).setRegistryName("minecraft:cobbled_deepslate")
-        );
-    }
+    public static void registerBlocks(RegisterEvent event) {
+        event.register(ForgeRegistries.Keys.BLOCKS, helper -> {
 
-    //REGISTERING ALL ITEMS
-    @ObjectHolder("minecraft:deepslate")
-    public static final Item DEEPSLATE_ITEM = null;
+            helper.register(new ResourceLocation("minecraft", "deepslate"), new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.DEEPSLATE).requiresCorrectToolForDrops().strength(1.5F, 6.0F).sound(SoundType.DEEPSLATE)));
 
-    @ObjectHolder("minecraft:cobbled_deepslate")
-    public static final Item COBBLED_DEEPSLATE_ITEM = null;
+            helper.register(new ResourceLocation("minecraft", "cobbled_deepslate"), new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).strength(1.5F, 6.0F)));
 
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event){
-        event.getRegistry().registerAll(
-                new BlockItem(DEEPSLATE_BLOCK, new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)).setRegistryName("minecraft:deepslate"),
-                new BlockItem(COBBLED_DEEPSLATE_BLOCK, new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)).setRegistryName("minecraft:cobbled_deepslate")
-        );
+        });
+
+        event.register(ForgeRegistries.Keys.ITEMS, helper -> {
+
+            helper.register(new ResourceLocation("minecraft", "deepslate"), new BlockItem(DEEPSLATE_BLOCK, new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+
+            helper.register(new ResourceLocation("minecraft", "cobbled_deepslate"), new BlockItem(COBBLED_DEEPSLATE_BLOCK, new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
+
+        });
     }
 }
