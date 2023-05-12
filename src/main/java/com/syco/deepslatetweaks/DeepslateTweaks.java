ac15ac8f59f91b84774a0dbe23244d1d0c46f909
Peter Pulcini <peter.pulcini89@gmail.com>
2023-05-12 16:54:38 -0400

    Newly Created 1.17.1

--- src/main/java/com/syco/deepslatetweaks/DeepslateTweaks.java
+++ src/main/java/com/syco/deepslatetweaks/DeepslateTweaks.java
@@ -1,17 +1,14 @@
 package com.syco.deepslatetweaks;
 
-import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(DeepslateTweaks.MOD_ID)
 public class DeepslateTweaks
 {
     public static final String MOD_ID = "deepslatetweaks";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public DeepslateTweaks()
     {
@@ -24,6 +21,6 @@ public class DeepslateTweaks
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        LOGGER.info("DEEPSLATE TWEAKS");
+
     }
 }
