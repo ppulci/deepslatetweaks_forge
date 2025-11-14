8bc3f16a38c071801195128bc9a15bf6df19139e
Peter Pulcini <peter.pulcini89@gmail.com>
2025-11-13 22:58:57 -0500

    Updated to 1.21.5

--- src/main/java/com/syco/deepslatetweaks/DeepslateTweaks.java
+++ src/main/java/com/syco/deepslatetweaks/DeepslateTweaks.java
@@ -2,10 +2,8 @@ package com.syco.deepslatetweaks;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(DeepslateTweaks.MOD_ID)
--- src/main/java/com/syco/deepslatetweaks/mixin/BlocksMixin.java
+++ src/main/java/com/syco/deepslatetweaks/mixin/BlocksMixin.java
@@ -1,17 +1,13 @@
 package com.syco.deepslatetweaks.mixin;
 
 import net.minecraft.world.level.block.Blocks;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Blocks.class)
